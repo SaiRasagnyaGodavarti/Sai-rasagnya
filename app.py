23a16c43a06425e90110f2c674c073966a560848
SaiRasagnyaGodavarti <65440205+SaiRasagnyaGodavarti@users.noreply.github.com>
2021-08-09 11:36:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     html = "<h3>Hello, All Welcome to GCP</h3>"
-    html = "<h3>Sai Rasagnya</h3>"
     return html
 
 if __name__ == "__main__":
